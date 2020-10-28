219342d5d133deaaab5acd15076f3075ac82f8ff
leshafatin <70691537+leshafatin@users.noreply.github.com>
2020-10-29 00:48:41 +0300

    Реализована фукнция if_prime()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,8 +13,10 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    d = 2
+    while d * d <= n and n % d != 0:
+        d += 1
+    return d * d > n
 
 
 def gcd(a: int, b: int) -> int:
