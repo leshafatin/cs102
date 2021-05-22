8d11005eeb489477b8e11a319961ed4a5af61a39
leshafatin <70691537+leshafatin@users.noreply.github.com>
2021-05-22 15:48:37 +0300

    Add files via upload

---
+++ homework06/naive_bayes/stemmer.py
@@ -0,0 +1,20 @@
+import typing as tp
+import nltk
+
+
+def clear(s):
+    try:
+        nltk.data.find("punkt")
+    except LookupError:
+        nltk.download("punkt", quiet=True)
+    try:
+        nltk.data.find("stopwords")
+    except LookupError:
+        nltk.download("stopwords", quiet=True)
+    stemmer = nltk.stem.SnowballStemmer("english")
+    tokens = [
+        stemmer.stem(token)
+        for token in nltk.word_tokenize(s)
+        if token.isalnum() and not token in nltk.corpus.stopwords.words("english")
+    ]
+    return " ".join(tokens)
